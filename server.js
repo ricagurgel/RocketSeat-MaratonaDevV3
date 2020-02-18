554a48796543535853c4a623e7dbde3af1e79593
Ricardo Gurgel <github_rg@dad.com.br>
2020-02-18 19:49:08 -0300

    tudo funcionando

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 require('dotenv').config()
 
 //postgre
-const pool = require('pg').Pool 
-const db = new pool({
+const { Pool } = require('pg') 
+const db = new Pool({
     user: process.env.dbuser,
     password: process.env.dbpass,
     host: process.env.dbhost,
     port: process.env.dbport,
     database: process.env.dbdb
 })
+ 
 
 // Configurar servidor
 const express = require('express')
@@ -29,19 +30,10 @@ nunjucks.configure('./', {
 })
 
 // Isto é um  Array
-const donors = [
-    {   name: "Flavio Gurgel",
-        blood: "O+"
-    },
-    {   name: "Marta Suplici",
-        blood: "A+"
-    },
-    {   name: "Muricy Santana",
-        blood: "AB+"
-    },
-    {   name: "Miguel Fernandes",
-        blood: "A+"
-    },
+var donors = [
+    {   name: "ops!",
+        blood: ""
+    }
 ]
 
 
@@ -56,14 +48,28 @@ server.listen(port, function(a,b,c){
 
 // Apresentar página (no caso, renderiza a index.html, usando nunjucks)
 server.get('/', function (req, res, next) {
-    return res.render('index.html', { donors })
+    db.query('select * from donors', function(err,result){
+        if(err) {res.send('erro no db')} 
+        var donors = result.rows
+        return res.render('index.html', { donors })
+    })
+
 })
 
 
 server.post('/', function (req,res,next){
-    var name = req.body.name
+    
+    var name = req.body.name 
     var blood = req.body.blood
     var email = req.body.email
+    if (name,blood,email ==""){
+        console.log('não pode estar vazio')
+        return res.send('Todos campos são obrigatórios')
+    }
+    var  query = `insert into donors ("name","blood","email") values ('${name}','${blood}','${email}')`
+    db.query(query, function(err){
+        if (err) return res.send('erro no banco de dados')
+    })
     donors.push({
         name: name,
         blood: blood
