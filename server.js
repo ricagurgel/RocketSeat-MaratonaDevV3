561783fb37d33c9f8336ed3495335d30d8b00634
Ricardo Gurgel <github_rg@dad.com.br>
2020-02-18 19:59:00 -0300

    atualizado as variáveis do .env para um modelo mais padrão

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ require('dotenv').config()
 //postgre
 const { Pool } = require('pg') 
 const db = new Pool({
-    user: process.env.dbuser,
-    password: process.env.dbpass,
-    host: process.env.dbhost,
-    port: process.env.dbport,
-    database: process.env.dbdb
+    user: process.env.PG_USER,
+    password: process.env.PG_PASS,
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DB
 })
  
 
